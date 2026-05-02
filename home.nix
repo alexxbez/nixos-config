641eb93009d8c96e96f694189201b2ea7b366607
alexxbez <alexxbez@proton.me>
2026-05-02 15:37:02 -0600

    updated fish

--- home.nix
+++ home.nix
@@ -461,6 +461,9 @@
     shellAliases = {
       ls = "eza --icons";
     };
+   interactiveShellInit = ''
+      set -gx LD_LIBRARY_PATH /run/current-system/sw/share/nix-ld/lib $LD_LIBRARY_PATH
+    '';
   };
 
   programs.zsh = {
